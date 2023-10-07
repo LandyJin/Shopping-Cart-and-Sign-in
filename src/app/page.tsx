3ce524c5c527c7e5351a255c79485c4a642e14b7
JIN HUIYAN <landy@JINs-MBP.gateway>
2023-10-07 17:46:40 +1100

    Add search products debounce function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 'use client'
 import ProductsList from "@/components/products/productsList"
 import { useGenerationStore } from "@/models/states"
+import { Product } from "@/models/types"
+import { useState } from "react"
+import debounce from 'lodash.debounce'
 
 // import 'bootstrap/dist/css/bootstrap.css';
 export default function Home() {
   const products = useGenerationStore((state) => state.products)
+  const [searchProducts, setSearchProducts] = useState<Product[]>(products)
+  function searchProductHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.value) {
+      setSearchProducts(products.filter(product => product.title.toLowerCase().includes(e.target.value.toLowerCase()) ))
+    } else {
+      setSearchProducts(products)
+    }
+    console.log(searchProducts)
+  }
+  const debounceSearch = debounce(searchProductHandler, 500)
   return (
       <>
-          <ProductsList products={products}/>
+        <input type="text" placeholder="Search product..." onChange={debounceSearch}/>
+        <ProductsList products={searchProducts}/>
       </>
   )
 }
